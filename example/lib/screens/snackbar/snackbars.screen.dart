71ed7d1f342e194876bec116dc32745ff4ba0c87
withcenterdev5 <withcenter.dev5@gmail.com>
2024-05-24 16:39:35 +0800

    snackbar update

--- example/lib/screens/snackbar/snackbars.screen.dart
+++ example/lib/screens/snackbar/snackbars.screen.dart
@@ -1,15 +1,26 @@
-import 'package:example/screens/snackbar/comic.snackbar.screen.dart';
-import 'package:example/screens/snackbar/sleek.snackbar.screen.dart';
 import 'package:flutter/material.dart';
 import 'package:social_design_system/social_design_system.dart';
 
-class SnackBarScreen extends StatelessWidget {
+class SnackBarScreen extends StatefulWidget {
   const SnackBarScreen({super.key});
+
+  @override
+  State<SnackBarScreen> createState() => _SnackBarScreenState();
+}
+
+class _SnackBarScreenState extends State<SnackBarScreen> {
+  bool isComic = true;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Theme(
-      data: ComicTheme.of(context),
+      data: isComic ? ComicTheme.of(context) : SleekTheme.of(context),
       child: Scaffold(
         appBar: AppBar(
           title: const Text('SnackBars'),
@@ -19,34 +30,62 @@ class SnackBarScreen extends StatelessWidget {
             padding: const EdgeInsets.all(24.0),
             child: Wrap(
               children: [
+                Theme(
+                  data: ComicTheme.of(context),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        isComic = true;
+                        ScaffoldMessenger.of(context).hideCurrentSnackBar(
+                            reason: SnackBarClosedReason.hide);
+                      });
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('This is a Comic SnackBar'),
+                          margin: EdgeInsets.all(24),
+                          behavior: SnackBarBehavior.floating,
+                        ),
+                      );
+                    },
+                    child: const Text('Comic Style Snackbar'),
+                  ),
+                ),
+                Theme(
+                  data: SleekTheme.of(context),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        isComic = false;
+                        ScaffoldMessenger.of(context).hideCurrentSnackBar(
+                            reason: SnackBarClosedReason.hide);
+                      });
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('This is a Sleek SnackBar'),
+                          margin: EdgeInsets.all(24),
+                          behavior: SnackBarBehavior.floating,
+                        ),
+                      );
+                    },
+                    child: const Text('Comic Style Snackbar'),
+                  ),
+                ),
                 // ElevatedButton(
-                //   onPressed: () {
-                //     ScaffoldMessenger.of(context).showSnackBar(
-                //       const SnackBar(
-                //         content: Text('This is a SnackBar'),
-                //         margin: EdgeInsets.all(24),
-                //         behavior: SnackBarBehavior.floating,
-                //       ),
-                //     );
-                //   },
+                //   onPressed: () => Navigator.of(context).push(
+                //     MaterialPageRoute(
+                //       builder: (_) => const ComicSnackBarsScreen(),
+                //     ),
+                //   ),
                 //   child: const Text('Comic Style Snackbar'),
                 // ),
-                ElevatedButton(
-                  onPressed: () => Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (_) => const ComicSnackBarsScreen(),
-                    ),
-                  ),
-                  child: const Text('Comic Style Snackbar'),
-                ),
-                ElevatedButton(
-                  onPressed: () => Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (_) => const SleekSnackBarScreen(),
-                    ),
-                  ),
-                  child: const Text('Sleek Style Snackbar'),
-                ),
+                // ElevatedButton(
+                //   onPressed: () => Navigator.of(context).push(
+                //     MaterialPageRoute(
+                //       builder: (_) => const SleekSnackBarScreen(),
+                //     ),
+                //   ),
+                //   child: const Text('Sleek Style Snackbar'),
+                // ),
               ],
             ),
           ),
