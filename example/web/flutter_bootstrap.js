25491b691ea65c0c3e25d18b886fea3d06225f9e
JaeHo Song <thruthesky@gmail.com>
2024-05-24 13:51:19 +0800

    adding web

---
+++ example/web/flutter_bootstrap.js
@@ -0,0 +1,20 @@
+{{flutter_js}}
+{{flutter_build_config}}
+
+
+const loadingMessage = document.querySelector('#loading-message');
+loadingMessage.textContent = "Downloading flutter engine...";
+console.log("-> Loading flutter engine...");
+_flutter.loader.load({
+    onEntrypointLoaded: async function (engineInitializer) {
+        
+        console.log("-> Engin is downloaded now. Going to initialize it.");
+    loadingMessage.textContent = "Engin is downloaded now. Going to initialize it.";
+    const appRunner = await engineInitializer.initializeEngine();
+
+        console.log("-> Engin is initialized. Booting the flutter app.");
+    loadingMessage.textContent = "Engin is initialized. Booting the flutter app.";
+        
+           await appRunner.runApp();
+  }
+});
