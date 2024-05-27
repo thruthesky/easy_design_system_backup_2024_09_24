57f8743866934256caa540edd4ccc6e5f8d23370
JaeHo Song <thruthesky@gmail.com>
2024-05-27 11:28:21 +0800

    card update

--- example/lib/screens/card/card.screen.dart
+++ example/lib/screens/card/card.screen.dart
@@ -10,56 +10,59 @@ class CardScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Card'),
       ),
-      body: SizedBox(
-        width: double.infinity,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Table(children: const [
+          TableRow(
+            children: [
+              Text('Comic Theme'),
+              Text('Sleek Theme'),
+            ],
+          ),
+          TableRow(children: [
+            ComicTheme(
+              child: Card(
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: CardChild(),
+                ),
+              ),
+            ),
+            SleekTheme(
+              child: Card(
+                child: CardChild(),
+              ),
+            ),
+          ])
+        ]),
+      ),
+    );
+  }
+}
+
+class CardChild extends StatelessWidget {
+  const CardChild({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const ListTile(
+          title: Text('Title'),
+          subtitle: Text('Subtitle'),
+          leading: Icon(Icons.ac_unit),
+          trailing: Icon(Icons.ac_unit),
+        ),
+        Row(
           children: [
-            const Text('Comic Theme'),
-            Theme(
-              data: ComicTheme.of(context),
-              child: const Card(
-                child: Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Text('Card Comic Theme'),
-                ),
-              ),
-            ),
-            Theme(
-              data: ComicTheme.of(context),
-              child: const Card.outlined(
-                child: Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Text('Card Outlined Comic Theme'),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 24,
-            ),
-            const Text('Sleek Theme'),
-            Theme(
-              data: SleekTheme.of(context),
-              child: const Card(
-                child: Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Text('Card Sleek Theme'),
-                ),
-              ),
-            ),
-            Theme(
-              data: SleekTheme.of(context),
-              child: const Card.filled(
-                child: Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Text('Card Filled Sleek Theme'),
-                ),
-              ),
-            ),
+            const Spacer(),
+            ElevatedButton(onPressed: () {}, child: const Text("YES")),
+            ElevatedButton(onPressed: () {}, child: const Text("NO"))
           ],
         ),
-      ),
+      ],
     );
   }
 }
