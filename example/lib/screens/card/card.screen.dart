d660d186ebf329d6d7468e3052920825a50063d3
JaeHo Song <thruthesky@gmail.com>
2024-05-27 11:46:59 +0800

    update card

--- example/lib/screens/card/card.screen.dart
+++ example/lib/screens/card/card.screen.dart
@@ -1,3 +1,4 @@
+import 'package:example/widgets/code_to_learn.dart';
 import 'package:flutter/material.dart';
 import 'package:social_design_system/social_design_system.dart';
 
@@ -10,34 +11,70 @@ class CardScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Card'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: Table(children: const [
-          TableRow(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
             children: [
-              Text('Comic Theme'),
-              Text('Sleek Theme'),
+              Table(children: const [
+                TableRow(
+                  children: [
+                    Text('Comic Theme'),
+                    Text('Sleek Theme'),
+                  ],
+                ),
+                TableRow(children: [
+                  ComicTheme(
+                    child: Card(
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: CardChild(),
+                      ),
+                    ),
+                  ),
+                  SleekTheme(
+                    child: Card(
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: CardChild(),
+                      ),
+                    ),
+                  ),
+                ])
+              ]),
+              const CodeToLearn(md: '''
+## Card
+
+* Card has many properties including a background and a border which causes the UI look dirty.
+* By default, Card widget has a border in comic theme and if you add a ListTile inside it, the border will be doubled. You can do your own customization to make it look better.
+  Below is an example of a Card with a ListTile inside it.
+```dart
+Card(
+  child: Padding(
+    padding: EdgeInsets.all(8.0),
+    child: Column(
+      children: [
+        const ListTile(
+          title: Text('Title'),
+          subtitle: Text('Subtitle'),
+          leading: Icon(Icons.ac_unit),
+        ),
+        Row(
+          children: [
+            const Spacer(),
+            ElevatedButton(onPressed: () {}, child: const Text("YES")),
+            ElevatedButton(onPressed: () {}, child: const Text("NO"))
+          ],
+        ),
+      ],
+    ),
+  ),
+),
+```
+        '''),
             ],
           ),
-          TableRow(children: [
-            ComicTheme(
-              child: Card(
-                child: Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: CardChild(),
-                ),
-              ),
-            ),
-            SleekTheme(
-              child: Card(
-                child: Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: CardChild(),
-                ),
-              ),
-            ),
-          ])
-        ]),
+        ),
       ),
     );
   }
