d3c6cbc80e6fc4c1ad69670c33922ba34016cc0b
thruthesky <thruthesky@users.noreply.github.com>
2024-05-30 11:46:07 +0000

    deploy: 431732a851b21d24e7af67d1e05a38cb9fe5bd07

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -14,7 +14,7 @@ const loadingMessage = document.querySelector('#loading-message');
 loadingMessage.textContent = "Downloading flutter engine...";
 console.log("-> Loading flutter engine...");
 _flutter.loader.load({
-    serviceWorkerVersion: "2305514932",
+    serviceWorkerVersion: "319562179",
     onEntrypointLoaded: async function (engineInitializer) {
         
         console.log("-> Engin is downloaded now. Going to initialize it.");
