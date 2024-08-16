8969b8cbd28bd3c4cb345e8f9ded5ee859a7e7f3
JaeHo Song <thruthesky@gmail.com>
2024-08-16 23:40:20 +0800

    add new main

---
+++ example/lib/main.2.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  static const String routeName = '/Home';
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: const Column(
+        children: [
+          Text("Home"),
+        ],
+      ),
+    );
+  }
+}
